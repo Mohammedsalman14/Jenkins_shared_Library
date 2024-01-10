457a8909e1cda7e04470009d58f1f7a40da25f3a
mohammed salman <mohamedsalman140@gmail.com>
2024-01-10 11:53:41 +0530

    pass and user initial with another type

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,7 @@ def call(){
     echo 'build the docker image...'
     withCredentials([usernamePassword(credentialsId:'dockerhub-repo', usernameVariable:'DOCKER_USER', passwordVariable:'DOCKER_PAS')]) {
         sh 'docker build -t salman14/mydevfirst:2.0 .'
-        sh "echo $DOCKER_PAS| docker login -u $DOCKER_USER --password-stdin  "
+        sh "echo ${DOCKER_PAS}| docker login -u ${DOCKER_USER} --password-stdin  "
         sh 'docker push salman14/mydevfirst:2.0'
     }
 }
