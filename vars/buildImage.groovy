937ef54e92fbf08bc8820139c863853854e42cb0
mohammed salman <mohamedsalman140@gmail.com>
2024-01-10 12:05:28 +0530

    using the parameter

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,13 @@
 #!/usr/bin/env/ groovy
 
-def call(){
+def call(String imageName){
     echo 'build the docker image...'
     withCredentials([usernamePassword(credentialsId:'dockerhub-repo', usernameVariable:'DOCKER_USER', passwordVariable:'DOCKER_PAS')]) {
-        sh 'docker build -t salman14/mydevfirst:2.0 .'
+        sh "docker build -t ${imageName} ."
         sh "echo ${DOCKER_PAS}| docker login -u ${DOCKER_USER} --password-stdin  "
-        sh 'docker push salman14/mydevfirst:2.0'
+        sh "docker push ${imageName}"
     }
 }
+
+
+// salman14/mydevfirst:2.0
